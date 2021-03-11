3517dda9fe98c77a4099361b01de42d2c50f1824
Markus Fenske <iblue@gmx.net>
2021-03-11 21:24:31 +0100

    Add client

--- client.c
+++ client.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
   fprintf(stderr, "WARNING: This tool should be invoked by qrexec only.\n");
   fprintf(stderr, "\n");
-  fprintf(stderr, "Debugging Usage: %s <source-path>\n", argv[0]);
-  fprintf(stderr, "Serves the file system at <source-path> via stdin/stdout pipes");
+  fprintf(stderr, "Debugging Usage: %s <target-path>\n", argv[0]);
+  fprintf(stderr, "Mounts the file system received via stdin/stdout pipes to <target-path>");
   return 1;
 }
